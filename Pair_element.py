ab2dd0654e754a0ff83c9a08351aea2bdd11cc43
AmandaZ2106 <romeogcruz@outlook.com>
2025-11-12 18:22:41 +0000

    Pair_element

--- Pair_element.py
+++ Pair_element.py
@@ -7,4 +7,4 @@ class pair_element:
                 return(lookup[target-num],i)
             lookup[num]=i
 value=int(input("Enter value for which you want to make this search:"))
-print("index1=%d index2=%d"%pair_element().two_sum((10,20,30,40,50,60,70),value))
+print("index1=%d index2=%d" %pair_element().two_sum((10,20,30,40,50,60,70),value))
